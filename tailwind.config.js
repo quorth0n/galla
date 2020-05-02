c33a882e53afcc381425ae18679468695743154a
whirish <lpoflynn@protonmail.ch>
2020-05-01 20:57:01 -0700

    Color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        "secondary-soft": "var(--color-secondary-soft)",
+        accent: "var(--color-accent)",
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
