f084f2ca497d49fbad90af86e31fd3af6a305209
whirish <lpoflynn@protonmail.ch>
2020-06-27 15:25:52 -0700

    Add privacy-friendly analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ToastProvider } from 'react-toast-notifications';
 import Amplify, { API, Auth, Storage } from 'aws-amplify';
+import Analytics from 'analytics';
+import simpleAnalyticsPlugin from '@analytics/simple-analytics';
 
 import Layout from '../components/layout/Layout';
 import awsconfig from '../src/aws-exports';
@@ -12,6 +14,11 @@ Amplify.register(API);
 Amplify.register(Auth);
 Amplify.register(Storage);
 Amplify.configure(awsconfig);
+
+Analytics({
+  app: 'galla',
+  plugins: [simpleAnalyticsPlugin()],
+});
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
